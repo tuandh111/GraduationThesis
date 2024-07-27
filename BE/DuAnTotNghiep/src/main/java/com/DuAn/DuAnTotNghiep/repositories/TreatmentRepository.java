a1fe3cd639f7be4c4f93ef6e5142172aeef992c9
TrungAug <trung2894@gmail.com>
2024-07-27 13:50:44 +0700

    update treatment api

--- BE/DuAnTotNghiep/src/main/java/com/DuAn/DuAnTotNghiep/repositories/TreatmentRepository.java
+++ BE/DuAnTotNghiep/src/main/java/com/DuAn/DuAnTotNghiep/repositories/TreatmentRepository.java
@@ -3,8 +3,31 @@ package com.DuAn.DuAnTotNghiep.repositories;
 import com.DuAn.DuAnTotNghiep.entities.Abnormality;
 import com.DuAn.DuAnTotNghiep.entities.Treatment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TreatmentRepository extends JpaRepository<Treatment,Integer> {
+
+    @Query("SELECT DISTINCT t " +
+            "FROM Treatment t " +
+            "JOIN IssuesTreatmentAutomation ita ON ita.treatment.treatmentId=t.treatmentId " +
+            "JOIN  DentalIssues di ON di.dentalIssuesId=ita.dentalIssues.dentalIssuesId " +
+            "WHERE t.isDeleted = false " +
+            "AND di.dentalIssuesId IN :ids " +
+            "ORDER BY t.treatmentId DESC ")
+    List<Object> getTreatmentByDentalIssues(@Param("ids") List<Integer> ids);
+
+
+    @Query("SELECT DISTINCT s " +
+            "FROM Service s " +
+            "JOIN ServiceTreatment st ON st.service.serviceId=s.serviceId " +
+            "JOIN  Treatment t ON t.treatmentId=st.treatment.treatmentId " +
+            "WHERE s.isDeleted = false " +
+            "AND t.treatmentId IN :ids " +
+            "ORDER BY s.serviceId DESC ")
+    List<Object> getServiceByTreatment(@Param("ids") List<Integer> ids);
 }
--- BE/DuAnTotNghiep/src/main/java/com/DuAn/DuAnTotNghiep/service/service/TreatmentService.java
+++ BE/DuAnTotNghiep/src/main/java/com/DuAn/DuAnTotNghiep/service/service/TreatmentService.java
@@ -5,6 +5,7 @@ import com.DuAn.DuAnTotNghiep.entities.Treatment;
 import com.DuAn.DuAnTotNghiep.model.request.ShiftRequest;
 import com.DuAn.DuAnTotNghiep.model.request.TreatmentRequest;
 import com.DuAn.DuAnTotNghiep.model.response.MessageResponse;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -22,4 +23,8 @@ public interface TreatmentService {
     MessageResponse delete(int treatmentId);
 
     MessageResponse softDeleteTreatment(int treatmentId);
+
+    List<Object> findTreatmentByDentalIssues(List<Integer> ids);
+
+    List<Object> findServiceByTreatment(List<Integer> ids);
 }
