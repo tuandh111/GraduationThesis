de6e7e52f351a902ad2f65ac0e0c21036516df06
tuandh12345 <129180520+tuandh12345@users.noreply.github.com>
2024-08-13 16:49:53 +0700

    Update PatientAndUserRequest.java

--- BE/DuAnTotNghiep/src/main/java/com/DuAn/DuAnTotNghiep/model/request/PatientAndUserRequest.java
+++ BE/DuAnTotNghiep/src/main/java/com/DuAn/DuAnTotNghiep/model/request/PatientAndUserRequest.java
@@ -1,10 +1,9 @@
 package com.DuAn.DuAnTotNghiep.model.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 import java.util.Date;
 
@@ -12,18 +11,34 @@ import java.util.Date;
 @Builder
 public class PatientAndUserRequest {
 
+
+    @NotEmpty(message = "Full name is required")
+    @Size(max = 100, message = "Full name should not exceed 100 characters")
+    @Pattern(regexp = "^[a-zA-Z\\s'-]+$", message = "Full name should only contain letters, spaces, hyphens, or apostrophes")
     private String fullName;
 
+    @NotEmpty(message = "Phone number is required")
+    @Pattern(regexp = "^(\\+\\d{1,3}[- ]?)?\\d{10}$", message = "Phone number should be valid and contain 10 digits")
     private String phoneNumber;
 
+    @NotEmpty(message = "Citizen Identification Number is required")
+    @Size(min = 9, max = 12, message = "Citizen Identification Number should be between 9 and 12 characters")
     private String citizenIdentificationNumber;
 
+    @NotNull(message = "Birthday is required")
+    @Past(message = "Birthday must be a past date")
     private Date birthday;
 
+    @NotEmpty(message = "Image URL is required")
+    @URL(message = "Image URL should be valid")
     private String imageURL;
 
+    @NotEmpty(message = "Gender is required")
+    @Pattern(regexp = " MALE| FEMALE|UNISEX", message = "Gender should be Male, Female, or Other")
     private String gender;
 
+    @NotEmpty(message = "Address is required")
+    @Size(max = 255, message = "Address should not exceed 255 characters")
     private String address;
 
     @NotEmpty(message = "Email is required")
